29c5a93e08f22ad5a849a6b15e98de0f75f90c4b
Sola Samuel <solasamuel@Solas-MacBook-Pro.local>
2024-08-11 15:13:26 +0100

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
 
 type Props = {}
 
 const App = (props: Props) => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
