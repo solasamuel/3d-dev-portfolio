eee94c865098a187f0c59da39edada6ec655509e
Sola Samuel <solasamuel@Solas-MacBook-Pro.local>
2024-10-10 23:18:47 +0100

    added work experience and tech

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -25,6 +25,8 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import adas from "./company/adas.png";
+import nyura from "./company/nyura.png"
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -59,4 +61,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  adas,
+  nyura
 };
--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber';
+import { 
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture
+} from "@react-three/drei";
 
-type Props = {}
+import CanvasLoader from "../Loader";
 
-const Ball = (props: Props) => {
-  return (
-    <div>Ball</div>
-  )
+type Props = {
+  imgUrl: string
 }
 
-export default Ball
+const Ball = (props: Props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#fff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  );
+}
+
+
+type BallCanvasProps = {
+  icon: string
+}
+
+const BallCanvas = (props: BallCanvasProps) => {
+  return (
+    <Canvas
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={props.icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+}
+
+
+export default BallCanvas;
